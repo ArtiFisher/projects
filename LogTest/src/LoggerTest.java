3c470896821f688cd089e768cc2afb450c0a620f
Fisher <artemis.fisher@gmail.com>
2013-12-10 22:17:37 +0300

    task2.2

---
+++ LogTest/src/LoggerTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Artem
+ * Date: 10.12.13
+ * Time: 20:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class LoggerTest {
+}
