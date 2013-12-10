22fab3331328ad0f160af5b6c2444367c1bcb0cb
Fisher <artemis.fisher@gmail.com>
2013-12-10 22:33:26 +0300

    task2.2

--- M4ALab2/src/M4ALab2_1.java
+++ M4ALab2/src/M4ALab2_1.java
@@ -8,26 +8,74 @@ public class M4ALab2_1 {
         return 1-t*t;
     }
 
+    static double ux0(double x){
+        return 3*x*x*x+x+1;
+    }
+
+    static double u1t(double t){
+        return 5+t;
+    }
+
+    static double du0t(double t){
+        return t+1;
+    }
+
+    static double a(double x){
+        return 5*x;
+    }
+
+    static double fi(double x, double t){
+        return 90*x-t*t-1;
+    }
+
     static double N=1, T=1;
     static double h=0.01, tau=0.01;
 
     public static void main(String[] args) {
-        double y[][]=new double[(int)(N/h)+1][(int)(T/tau)+1];
-        for(int i=0;i<N/h+1;i++)
-            y[0][i]=mu(i*tau);
+        //Task1
+//        double y[][]=new double[(int)(N/h)+1][(int)(T/tau)+1];
+//        for(int i=0;i<T/tau+1;i++)
+//            y[0][i]=mu(i*tau);
+//
+//        for(int i=0;i<N/h+1;i++)
+//            y[i][0]=u0(i*h);
+//
+//        for(int n=0;n<T/tau;n++){
+//            for(int i=1; i<(int)(N/h); i++)
+//               y[i][n+1]=(y[i][n]/tau-(y[i][n]-y[i-1][n]-y[i-1][n+1])/2/h)/(1/tau-1/2/h);
+//        }
+//
+//        for(int n=0;n<T/tau+1;n++){
+//            for(int i=0; i<N/h+1; i++)
+//                System.out.print(y[i][n]+"\t");
+//            System.out.println();
+//        }
 
+        //Task2
+        h=0.1;
+        tau=0.001;
+        N=1;
+        T=0.5;
+        double y[][]=new double[(int)(N/h)+1][(int)(T/tau)+1];
         for(int i=0;i<T/tau+1;i++)
-            y[i][0]=u0(i*h);
+            y[(int)(N/h)][i]=u1t(i*tau);
+
+        for(int i=0;i<N/h+1;i++)
+            y[i][0]=ux0(i*h);
+
+        //how to initialize left bound with du0t?
+        //for(int i=0;i<T/tau+1;i++)
+            //y[0][i]=?;
+
 
         for(int n=0;n<T/tau;n++){
-            for(int i=1; i<(int)(N/h); i++)
-               y[i][n+1]=(y[i][n]/tau-(y[i][n]-y[i-1][n]-y[i-1][n+1])/2/h)/(1/tau-1/2/h);
+            for(int i=(int)(N/h)-1; i>0; i--)
+                y[i][n+1]=tau*(a(i*h)*(y[i+1][n]-2*y[i][n]+y[i-1][n])/h/h+y[i][n]/tau-fi(i*h, n*tau));
         }
 
         for(int n=0;n<T/tau+1;n++){
-            System.out.print(n+"  ");
             for(int i=0; i<N/h+1; i++)
-                System.out.print(y[i][n]+"  ");
+                System.out.print(y[i][n]+"\t");
             System.out.println();
         }
 
