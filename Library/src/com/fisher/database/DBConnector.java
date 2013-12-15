e2a7d2d1e9c763050e32acebe52f49219c460e34
ArtiFisher <artemis.fisher@gmail.com>
2013-12-15 21:27:23 +0300

    librarianDAO

---
+++ Library/src/com/fisher/database/DBConnector.java
@@ -0,0 +1,123 @@
+package com.fisher.database;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.fisher.database.resource.Resource;
+import org.apache.log4j.Logger;
+
+public class DBConnector {
+
+    public static final String JDBS_CONTEXT_PATH = "jdbc.context.path";
+    public static final String MSG_CONNECTION_NULL = "error.connection.null";
+    public static final String MSG_STATEMENT_NULL = "error.statement.null";
+    /**
+     * This is logger which print some messages to log file
+     */
+    private Logger logger = Logger.getLogger(DBConnector.class);
+    /**
+     * This is connection with database
+     */
+    private Connection connection;
+    /**
+     * This is statement
+     */
+    private Statement statement;
+    /**
+     * This is prepared statement
+     */
+    private PreparedStatement preparedStatement;
+
+    /**
+     * This method gets a connection from connection pool
+     */
+    public DBConnector() {
+        try {
+            Context initCtx = (Context) new InitialContext();
+            DataSource ds = (DataSource) initCtx.lookup(Resource.getStr(JDBS_CONTEXT_PATH));
+            connection = ds.getConnection();
+            statement = connection.createStatement();
+        } catch (SQLException ex) {
+            logger.error(ex);
+        } catch (NamingException ex) {
+            logger.error(ex);
+        }
+    }
+
+    /**
+     * This method returns a connection
+     * @return a connection
+     * @throws SQLException a exception if connection is null
+     */
+    public Connection getConnection() throws SQLException {
+        if (connection != null) {
+            return connection;
+        } else {
+            throw new SQLException(Resource.getStr(MSG_CONNECTION_NULL));
+        }
+    }
+
+    /**
+     * This method returns a prepared statement
+     * @return a prepared statement
+     * @throws SQLException a exception if prepared statement is null
+     */
+    public PreparedStatement getPreparedStatement(String sql) throws SQLException {
+        if (connection != null) {
+            preparedStatement = connection.prepareStatement(sql);
+            if (preparedStatement != null) {
+                return preparedStatement;
+            }
+        }
+        throw new SQLException(Resource.getStr(MSG_STATEMENT_NULL));
+    }
+
+    /**
+     * This method returns a statement
+     * @return a statement
+     * @throws SQLException a exception if statement is null
+     */
+    public Statement getStatement() throws SQLException {
+        if (connection != null) {
+            statement = connection.createStatement();
+            if (statement != null) {
+                return statement;
+            }
+        }
+        throw new SQLException(Resource.getStr(MSG_STATEMENT_NULL));
+    }
+
+    /**
+     * This method tries to close snachalf statement and prapered statement but
+     * afterwards and connection
+     */
+    public void close() {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException ex) {
+                logger.error(ex);
+            }
+        }
+        if (preparedStatement != null) {
+            try {
+                preparedStatement.close();
+            } catch (SQLException ex) {
+                logger.error(ex);
+            }
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                logger.error(ex);
+            }
+        }
+    }
+}
--- Library/web/index.jsp
+++ Library/web/index.jsp
@@ -1,16 +1,10 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: Artem
-  Date: 10.12.13
-  Time: 12:07
-  To change this template use File | Settings | File Templates.
---%>
+
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
     <title></title>
   </head>
   <body>
-
+     something
   </body>
 </html>
