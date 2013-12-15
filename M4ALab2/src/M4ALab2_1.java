8e39f8040f9c550312dbb1bc6a462caa5b9a1f44
Fisher <artemis.fisher@gmail.com>
2013-12-15 11:49:20 +0300

    plus2graph

--- M4ALab2/src/M4ALab2_1.java
+++ M4ALab2/src/M4ALab2_1.java
@@ -29,51 +29,51 @@ public class M4ALab2_1 {
     }
 
     static double N=1, T=1;
-    static double h=0.01, tau=0.01;
+    static double h=0.01, tau=0.02;
 
     public static void main(String[] args) {
-        //Task1
-//        double y[][]=new double[(int)(N/h)+1][(int)(T/tau)+1];
-//        for(int i=0;i<T/tau+1;i++)
-//            y[0][i]=mu(i*tau);
-//
-//        for(int i=0;i<N/h+1;i++)
-//            y[i][0]=u0(i*h);
-//
-//        for(int n=0;n<T/tau;n++){
-//            for(int i=1; i<(int)(N/h); i++)
-//               y[i][n+1]=(y[i][n]/tau-(y[i][n]-y[i-1][n]-y[i-1][n+1])/2/h)/(1/tau-1/2/h);
-//        }
-//
-//        for(int n=0;n<T/tau+1;n++){
-//            for(int i=0; i<N/h+1; i++)
-//                System.out.print(y[i][n]+"\t");
-//            System.out.println();
-//        }
+//        Task1
+        double y[][]=new double[(int)(N/h)+1][(int)(T/tau)+1];
+        for(int i=0;i<T/tau+1;i++)
+            y[0][i]=mu(i*tau);
 
-        //Task2
-        h=0.1;
-        tau=0.001;
-        N=1;
-        T=0.5;
-        double y[][]=new double[(int)(N/h)][(int)(T/tau)];
-        for(int i=0;i<T/tau;i++)
-            y[(int)(N/h)-1][i]=u1t(i*tau);
-
-        for(int i=0;i<N/h;i++)
-            y[i][0]=ux0(i*h);
-
-        for(int n=0;n<T/tau-1;n++){
-            for(int i=(int)(N/h)-2; i>0; i--)
-                y[i][n+1]=(tau*a(i*h)*(y[i+1][n]-2*y[i][n]+y[i-1][n])/h/h+y[i][n]-tau*fi(i*h, n*tau));
-            y[0][n]=y[1][n]-du0t(n*tau)*h;
-        }
+        for(int i=0;i<N/h+1;i++)
+            y[i][0]=u0(i*h);
 
         for(int n=0;n<T/tau;n++){
+            for(int i=1; i<(int)(N/h); i++)
+               y[i][n+1]=(y[i][n]/tau-(y[i][n]-y[i-1][n]-y[i-1][n+1])/2/h)/(1/tau-1/2/h);
+        }
+
+        for(int n=0;n<T/tau+1;n++){
             for(int i=0; i<N/h; i++)
                 System.out.print(y[i][n]+"\t");
             System.out.println();
         }
+
+//        //Task2
+//        h=0.1;
+//        tau=0.001;
+//        N=1;
+//        T=0.5;
+//        double y[][]=new double[(int)(N/h)][(int)(T/tau)];
+//        for(int i=0;i<T/tau;i++)
+//            y[(int)(N/h)-1][i]=u1t(i*tau);
+//
+//        for(int i=0;i<N/h;i++)
+//            y[i][0]=ux0(i*h);
+//
+//        for(int n=0;n<T/tau-1;n++){
+//            for(int i=(int)(N/h)-2; i>0; i--)
+//                y[i][n+1]=(tau*a(i*h)*(y[i+1][n]-2*y[i][n]+y[i-1][n])/h/h+y[i][n]-tau*fi(i*h, n*tau));
+//            y[0][n]=y[1][n]-du0t(n*tau)*h;
+//        }
+//
+//        for(int n=0;n<T/tau;n++){
+//            for(int i=0; i<N/h; i++)
+//                System.out.print(y[i][n]+"\t");
+//            System.out.println();
+//        }
 
     }
 }
